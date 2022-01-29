239dfc4c3e7bb48280b226a6aea84e963e869d59
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2022-01-29 23:45:45 +0200

    unimportant

--- scite.lua
+++
@@ -1,7 +0,0 @@
---convenience module to load when running Lua scripts with F5 from SciTE.
-io.stdout:setvbuf'no'
-io.stderr:setvbuf'no'
-pp = require'pp'
-require'terra'
-glue = require'glue'
-pr = terralib.printraw
